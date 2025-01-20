a3f78ea73a23911564a93a24ae67128207534bad
Atharva Chourikar <90090331+Atharvac31@users.noreply.github.com>
2025-01-20 20:15:05 +0530

    initialized banner

--- src/Components/CardHolder.jsx
+++ src/Components/CardHolder.jsx
@@ -3,67 +3,72 @@ import React from "react";
 import "./CardHolder.css";
 const CardHolder = () => {
   return (
-    <div className="cardDiv">
+    <div className="cardComponent">
       <div>
-        <img
-          src="https://codewithsadee.github.io/tourly/assets/images/popular-1.jpg"
-          width={"343px"}
-          height={"410px"}
-          style={{ borderRadius: "20px" }}
-        />
-        <div className="cardContent">
-          <div className="cardTitle">
-            <span>ITALY</span>
-          </div>
-          <div className="cardSubTitle">
-            <span>SAN MIGUEL</span>
-          </div>
-          <div className="cardText">
-            <span>
-              Fusce hic augue velit wisi ips quibusdam pariatur, iusto.
-            </span>
+        <span>Uncover place</span>
+      </div>
+      <div className="cardDiv">
+        <div>
+          <img
+            src="https://codewithsadee.github.io/tourly/assets/images/popular-1.jpg"
+            width={"343px"}
+            height={"410px"}
+            style={{ borderRadius: "20px" }}
+          />
+          <div className="cardContent">
+            <div className="cardTitle">
+              <span>ITALY</span>
+            </div>
+            <div className="cardSubTitle">
+              <span>SAN MIGUEL</span>
+            </div>
+            <div className="cardText">
+              <span>
+                Fusce hic augue velit wisi ips quibusdam pariatur, iusto.
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-      <div>
-        <img
-          src="https://codewithsadee.github.io/tourly/assets/images/popular-2.jpg"
-          width={"343px"}
-          height={"410px"}
-          style={{ borderRadius: "20px" }}
-        />
-        <div className="cardContent">
-          <div className="cardTitle">
-            <span>DUBAI</span>
-          </div>
-          <div className="cardSubTitle">
-            <span>BURJ KHALIFA</span>
-          </div>
-          <div className="cardText">
-            <span>
-              Fusce hic augue velit wisi ips quibusdam pariatur, iusto.
-            </span>
+        <div>
+          <img
+            src="https://codewithsadee.github.io/tourly/assets/images/popular-2.jpg"
+            width={"343px"}
+            height={"410px"}
+            style={{ borderRadius: "20px" }}
+          />
+          <div className="cardContent">
+            <div className="cardTitle">
+              <span>DUBAI</span>
+            </div>
+            <div className="cardSubTitle">
+              <span>BURJ KHALIFA</span>
+            </div>
+            <div className="cardText">
+              <span>
+                Fusce hic augue velit wisi ips quibusdam pariatur, iusto.
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-      <div>
-        <img
-          src="https://codewithsadee.github.io/tourly/assets/images/popular-3.jpg"
-          width={"343px"}
-          height={"410px"}
-          style={{ borderRadius: "20px" }}
-        />
-        <div className="cardContent">
-          <div className="cardTitle">
-            <span>JAPAN</span>
-          </div>
-          <div className="cardSubTitle">
-            <span>KYOTO TEMPLE</span>
-          </div>
-          <div className="cardText">
-            <span>
-              Fusce hic augue velit wisi ips quibusdam pariatur, iusto.
-            </span>
+        <div>
+          <img
+            src="https://codewithsadee.github.io/tourly/assets/images/popular-3.jpg"
+            width={"343px"}
+            height={"410px"}
+            style={{ borderRadius: "20px" }}
+          />
+          <div className="cardContent">
+            <div className="cardTitle">
+              <span>JAPAN</span>
+            </div>
+            <div className="cardSubTitle">
+              <span>KYOTO TEMPLE</span>
+            </div>
+            <div className="cardText">
+              <span>
+                Fusce hic augue velit wisi ips quibusdam pariatur, iusto.
+              </span>
+            </div>
           </div>
         </div>
       </div>
--- src/Routes/LandingPage.jsx
+++ src/Routes/LandingPage.jsx
@@ -1,9 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import CardHolder from "../Components/cardHolder";
+import Banner from "../Components/Banner";
 const LandingPage = () => {
   return (
     <div>
+      <Banner />
       <CardHolder />
     </div>
   );
